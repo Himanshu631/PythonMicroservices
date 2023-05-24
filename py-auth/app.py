3431a7b77c9a394923e1aa6c990b7a1fc5869ad1
Himanshu631 <himanshupauljsr@gmail.com>
2023-05-24 08:11:36 +0530

    added new UI of homepage

--- py-auth/app.py
+++ py-auth/app.py
@@ -1,10 +1,11 @@
-import jwt, datetime, os
-from flask_mysqldb import MySQL
+#import jwt, datetime, os
+import os
+#from flask_mysqldb import MySQL
 from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
-mysql = MySQL(app)
+#mysql = MySQL(app)
 
 # config
 app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
@@ -16,24 +17,24 @@ app.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
-@app.route('/login', methods=["POST"])
-def login():
-    auth = request.authorization
-    if not auth:
-        return "missing credentials", 401
+#@app.route('/login', methods=["POST"])
+#def login():
+#    auth = request.authorization
+#    if not auth:
+#        return "missing credentials", 401
+#
+#    cur = mysql.connection.cursor()
+#    res = cur.execute(
+#       "SELECT email,password FROM user WHERE email=%s", (auth.username,auth.password)
+#    )
 
-    cur = mysql.connection.cursor()
-    res = cur.execute(
-        "SELECT email,password FROM user WHERE email=%s", (auth.username,auth.password)
-    )
 
-
-@app.route('register')
-def register():
-    return render_template('register.html')
+#@app.route('register')
+#def register():
+#    return render_template('register.html')
 
 
 if __name__ == '__main__':
